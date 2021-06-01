e450aa7c9bf20d6ceb287a13ed4ff55efb9c1361
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-01 12:08:03 +0200

    Native

--- Program.cs
+++ Program.cs
@@ -2,17 +2,17 @@ using System;
 
 namespace Waifu2x_Vulkan_CSharp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Waifu2xWrapper waifu2x = new();
-            waifu2x.setInput("origin.jpg");
-            waifu2x.setNoise(2);
-            waifu2x.setScale(2);
-            waifu2x.setOutput("output.png");
-            Console.WriteLine(waifu2x.execute() == 0 ? "Successful" : "Failed");
-            waifu2x.Dispose();
+            Waifu2xWrapper waifu2X = new();
+            waifu2X.setInput("origin.jpg");
+            waifu2X.setNoise(2);
+            waifu2X.setScale(2);
+            waifu2X.setOutput("output.png");
+            Console.WriteLine(waifu2X.execute() == 0 ? "Successful" : "Failed");
+            waifu2X.Dispose();
         }
     }
 }
